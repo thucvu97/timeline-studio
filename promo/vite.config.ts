a451ff105660278bb45b864de74020b808a8d065
Alexander Kireev <ak.chatman.media@gmail.com>
2025-05-21 07:35:02 +0700

    fix: обновить версию Node.js до 20 и исправить экспорт в конфигурационных файлах

--- promo/vite.config.ts
+++ promo/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
   base: './',
 })
+
+export default config
