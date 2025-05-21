5e9e654b60320a54bbfa1dd61d8288c17a9712b0
Alexander Kireev <ak.chatman.media@gmail.com>
2025-05-21 08:04:13 +0700

    fix: обновить конфигурацию проекта, добавив поддержку Tailwind CSS и исправив тип модуля в package.json

--- promo/tailwind.config.cjs
+++ promo/tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#38dacac3',
+          dark: '#35d1c1',
+        },
+      },
+    },
   },
   plugins: [],
   future: {
