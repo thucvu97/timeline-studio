0566df5fb6b0955582a9e077d3bda127fa88cf1b
Alexander Kireev <ak.chatman.media@gmail.com>
2025-05-21 07:42:47 +0700

    fix: удалить неиспользуемую зависимость @tailwindcss/postcss и обновить версии TypeScript и Vite в package.json; упростить экспорт конфигурации Vite

--- promo/vite.config.ts
+++ promo/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-const config = defineConfig({
+export default defineConfig({
   plugins: [react()],
   base: './',
 })
-
-export default config
