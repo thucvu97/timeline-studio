de1bfbd97b3c8c1b4d56ad492dbe09946e99f275
Alexander Kireev <ak.chatman.media@gmail.com>
2025-05-23 09:06:13 +0700

    Исправлены ошибки линтера и скрипт генерации документации

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -5,9 +5,14 @@
  * Исключает тестовые файлы и файлы в директории test
  */
 
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получаем текущую директорию в ES модуле
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Создаем временный файл конфигурации TypeScript для TypeDoc
 const tsconfig = {
