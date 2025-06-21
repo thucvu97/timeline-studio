91f606f531c1ba829564cfe5b0392aac24842075
Alexander Kireev <ak.chatman.media@gmail.com>
2025-06-21 22:55:46 +0700

    chore: update version to 0.22.0 across various files and mocks

--- e2e/helpers/test-utils.ts
+++ e2e/helpers/test-utils.ts
@@ -110,7 +110,7 @@ export async function mockTauriAPI(page: Page) {
 
           switch (cmd) {
             case "get_app_info":
-              return { version: "0.21.0", name: "Timeline Studio" }
+              return { version: "0.22.0", name: "Timeline Studio" }
 
             case "get_media_files":
               return []
