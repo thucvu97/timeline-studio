bc00a413a510f75b1f15e52b8febf1508b9a882f
Alexander Kireev <ak.chatman.media@gmail.com>
2025-07-07 02:14:36 +0700

    cargo fmt

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,14 +6,14 @@ fn main() {
     std::env::remove_var("FFMPEG_DIR");
     std::env::remove_var("FFMPEG_INCLUDE_DIR");
     std::env::remove_var("FFMPEG_LIB_DIR");
-    
+
     // Set PKG_CONFIG to use system libraries
     if cfg!(target_os = "linux") {
       std::env::set_var("PKG_CONFIG_ALLOW_SYSTEM_LIBS", "1");
       std::env::set_var("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS", "1");
     }
   }
-  
+
   // Run the default Tauri build script
   tauri_build::build();
 }
